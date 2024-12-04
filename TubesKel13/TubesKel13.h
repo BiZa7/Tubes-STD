daecbbec23407b89b8e0a84e50fe9ed823e77a52
BiZa7 <binarmuhammadfaza@gmail.com>
2024-12-04 17:04:30 +0700

    Update TubesKel13.h

--- TubesKel13/TubesKel13.h
+++ TubesKel13/TubesKel13.h
@@ -51,5 +51,20 @@ struct ListParent
     adrP lastP;
 };
 
+void createListParent(ListParent &Pr);
+adrP createElmP(infotypeP dataP);
+bool isEmptyP(ListParent Pr);
+void insertFirstP(ListParent &Pr, adrP P);
+void insertLastP(ListParent &Pr, adrP P);
+void showP(ListParent Pr);
+void deleteFirstP(ListParent &Pr, adrP &P);
+void deleteLastP(ListParent &Pr, adrP &P);
+adrP searchP(ListParent Pr,string Kereta, string Kelas, int InGerbong);
+adrC createElmChild(infotypeC dataC);
+void insertCFirst(adrP &P, adrC C);
+
+void showAll(ListParent Pr);
+adrC searchChild(ListParent Pr, string Kereta, string Kelas, int InGerbong, string NamaPenumpang, string NoKursi, int HargaTiket );
+void deleteFirstChild(adrP &P, adrC &C, ListParent Pr,string Kereta, string Kelas, int InGerbong);
 
 #endif // TUBESKEL13_H_INCLUDED
